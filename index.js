a83568ae8548478b85ab475b630d3024bb4e0d1f
Tetiana <burlaka199327@gmail.com>
2023-11-25 16:16:55 +0200

    js-task-3

--- index.js
+++ index.js
@@ -26,7 +26,40 @@
  70 === 01:10
  */
 
-const number = prompt("Введіть кількість хвилин.");
-const hours = String(Math.floor(number / 60)).padStart(2, "0");
-const minutes = String(number % 60).padStart(2, "0");
-console.log(`${hours}:${minutes}`);
+// const number = prompt("Введіть кількість хвилин.");
+// const hours = String(Math.floor(number / 60)).padStart(2, "0");
+// const minutes = String(number % 60).padStart(2, "0");
+// console.log(`${hours}:${minutes}`);
+
+/**
+*? Напишіть код, який запитуватиме
+*? логін за допомогою prompt та логувати результат
+*? у консоль браузера
+    
+*? Якщо відвідувач вводить "Адмін",
+*? то prompt запитує пароль.
+*? Якщо нічого не ввели або натиснуто клавішу Esc
+*? вивести рядок "Скасовано"
+*? В іншому випадку вивести рядок "Я вас не знаю"
+    
+*? Пароль перевіряти так:
+*? Якщо введено пароль "Я головний",
+*? то вивести рядок "Здрастуйте!"
+*? інакше виводити рядок "Невірний пароль!"
+ */
+
+const login = prompt("Введіть логін.").trim();
+console.log(login);
+if (login === "Адмін") {
+  const password = prompt("Введіть пароль.");
+  if (password === "Я головний") {
+    console.log("Здрастуйте!");
+  } else {
+    console.log("Невірний пароль!");
+  }
+} else if (!login) {
+  // } else if (login === "" || login === null) {
+  console.log("Скасовано");
+} else {
+  console.log("Я вас не знаю");
+}
