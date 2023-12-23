3031dc1caa0860af0ed817d8eb3ee2f87217c894
Liubjchka <144176266+Liubjchka@users.noreply.github.com>
2023-12-23 15:42:52 +0200

    add task-2

--- index.js
+++ index.js
@@ -243,11 +243,34 @@ const fruits = [
   });
 }; */
 
-const newFruits = (arr) =>
-  arr.map((fruit, index) => ({
-    id: index + 1,
-    ...fruit,
-    price: fruit.price * 0.8,
-  }));
+// const newFruits = (arr) =>
+//   arr.map((fruit, index) => ({
+//     id: index + 1,
+//     ...fruit,
+//     price: fruit.price * 0.8,
+//   }));
 
-console.log(newFruits(fruits));
+// console.log(newFruits(fruits));
+
+/**
+ *? З об'єкту concerts потрібно отримати масив
+ *? в якому будуть лише імена міст.
+ *? З масиву потрібно прибрати міста, в яких концерт уже пройшов і
+ *? відсортувати їх у хронологічному порядку.
+ *? Результат вивести у консоль.
+ *? Очікуваний результат ["Одеса", "Умань", "Харків"]
+ */
+const concerts = {
+  Київ: new Date("2020-04-01"),
+  Умань: new Date("2025-07-02"),
+  Вінниця: new Date("2020-04-21"),
+  Одеса: new Date("2025-03-15"),
+  Хмельницький: new Date("2020-04-18"),
+  Харків: new Date("2025-07-10"),
+};
+
+const newArray = Object.keys(concerts);
+const softFilter = newArray.filter((element)=>{return concerts[element] > new Date()})
+const sortedFilter = softFilter.toSorted((a, b) => {return concerts[a] - concerts[b]})
+console.log(sortedFilter);
+console.log(new Date());
